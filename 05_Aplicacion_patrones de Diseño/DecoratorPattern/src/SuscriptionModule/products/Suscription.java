10de9744f5acfc79549f737358b7e535fdaa3ffc
GaboH97 <gabriel970826@gmail.com>
2017-09-21 09:18:04 -0500

    Agrego Documentación Suscription

--- 05_Aplicacion_patrones de Diseño/DecoratorPattern/src/SuscriptionModule/products/Suscription.java
+++ 05_Aplicacion_patrones de Diseño/DecoratorPattern/src/SuscriptionModule/products/Suscription.java
@@ -6,10 +6,13 @@ import java.math.BigDecimal;
 /**
  *
  * @author Cesar Cardozo, Gabriel Amaya
+ * 
+ * Descripción: Clase abstracta que define los atributos básicos de una suscripción para ser modificados por sus clases hijas
  */
 public abstract class Suscription implements ISuscriptionData{
-
+    
     public BigDecimal price;
     public String Name;
     public String description;
+    
 }
