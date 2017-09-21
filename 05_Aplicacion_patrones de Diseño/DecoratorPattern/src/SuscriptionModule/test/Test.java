5bfe62f89f79ac92e4ee0735e4b0bc56892460a5
GaboH97 <gabriel970826@gmail.com>
2017-09-21 09:45:21 -0500

    Agrego pruebas Test

--- 05_Aplicacion_patrones de Diseño/DecoratorPattern/src/SuscriptionModule/test/Test.java
+++ 05_Aplicacion_patrones de Diseño/DecoratorPattern/src/SuscriptionModule/test/Test.java
@@ -3,26 +3,35 @@ package SuscriptionModule.test;
 import SuscriptionModule.decorators.ExecutiveCard;
 import SuscriptionModule.decorators.Group;
 import SuscriptionModule.decorators.MontlyPayment;
-import SuscriptionModule.decorators.Personal;
-import SuscriptionModule.decorators.Student;
+import SuscriptionModule.decorators.SportClubMember;
 import SuscriptionModule.products.ISuscriptionData;
 import SuscriptionModule.products.MonthlySuscription;
 import SuscriptionModule.products.MonthlySuscritionType;
 import SuscriptionModule.products.Session;
 
-
-
 /**
  *
- * @author Cesar Cardozo
+ * @author Cesar Cardozo, Gabriel Amaya 
+ * Descripción: Clase prueba del módulo de suscripciones aplicando patrón de diseño Decorator
  */
 public class Test {
 
     public static void main(String[] args) {
-        ISuscriptionData session = new ExecutiveCard(new Session(),5);
-        System.out.println(session.getName() +": "+ session.getDescription() + ": " +session.getPrice());
-        //este deveria dar un entrenamiento de 15 dias en grupo con precio de 36 barras.
-        ISuscriptionData month = new Group(new MontlyPayment(new MonthlySuscription(MonthlySuscritionType.WEIGHTLIFINTG),0));
-        System.out.println(month.getName() +": "+ month.getDescription() + ": " +month.getPrice());
+        System.out.println("PRUEBAS DE CONCEPTO MÓDULO DE SUSCRIPCIÓN UTILIZANDO PATRÓN DE DISEÑO DECORATOR");
+        //Crea una instancia de la clase Session que es decorada con la opción de tarjeta ejecutiva de 5 sesiones
+        
+        ISuscriptionData suscription2 = new ExecutiveCard(new Session(),5);
+        System.out.println(suscription2.getName() +": "+ suscription2.getDescription() + ": " +suscription2.getPrice());
+        
+        // Crea una instancia de la clase MontlySuscription de tipo pesas que se
+        // Decora para que ofrecerla en plan quincenal para grupos de dos o más
+        // personas
+        ISuscriptionData suscription1 = new Group(new MontlyPayment(new MonthlySuscription(MonthlySuscritionType.WEIGHTLIFINTG),0));
+        System.out.println(suscription1.getName() +": "+ suscription1.getDescription() + ": " +suscription1.getPrice());
+        
+        // Crea una instancia de la clase MontlySuscription general que se
+        // Decora para que ofrecerla en tipo membresía de un año para miembro de club deportivo
+        ISuscriptionData suscription3 = new SportClubMember(new MontlyPayment(new MonthlySuscription(MonthlySuscritionType.ALL)  , 12));
+         System.out.println(suscription3.getName() +": "+ suscription3.getDescription() + ": " +suscription3.getPrice());
     }
 }
