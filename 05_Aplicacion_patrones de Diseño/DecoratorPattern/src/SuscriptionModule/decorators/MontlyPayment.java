7c2639c44bd8684883a3a0cef570e8ca88a87ca6
CesarCardozo <srfeudalwow@gmail.com>
2017-09-21 09:19:53 -0500

    commit

--- 05_Aplicacion_patrones de Diseño/DecoratorPattern/src/SuscriptionModule/decorators/MontlyPayment.java
+++ 05_Aplicacion_patrones de Diseño/DecoratorPattern/src/SuscriptionModule/decorators/MontlyPayment.java
@@ -8,16 +8,34 @@ import java.math.RoundingMode;
 /**
  *
  * @author Cesar Cardozo, Gabriel Amaya
+ * Description: decorator para susripciones mensuales en el que deacuerdo a 
+ * la cantidad de meses se dara un descuento y saldra mas economico que comprar
+ * la misma cantidad pero de una en una
  */
 public class MontlyPayment extends Decorator {
 
+    /**
+    *
+    * dictara el numero de meses que se desean adquirir para una mensualidad, 
+    * este pordra ser 2, 3, 6, 12, o 0 para referirce a una suscripcion mensual pero
+    * que solo comprenda 15 dias.
+    */
     private int monthAmount;
 
+    /**
+    *
+    * @param mountAmount: dicta la cantidad de meses que se desean adquirr
+    */
     public MontlyPayment(ISuscriptionData isuscriptionM, int monthAmount) {
         super(isuscriptionM);
         this.monthAmount = monthAmount;
     }
 
+    /**
+    *
+    * @return: retorna el nombre de la suscripcion deacuerdo a la cantidad de meses
+    * deseados en monthAmount, en caso de ser 0 se refiere a 15 dias.
+    */
     @Override
     public String getName() {
         switch (monthAmount) {
@@ -35,7 +53,14 @@ public class MontlyPayment extends Decorator {
                 return Utils.ERROR_MESSAGE;
         }
     }
-
+    
+    /**
+    *
+    * @return: retorna el precio de la suscripcion deacuerdo a la cantidad de mesese
+    * deseados en monthAmount, para esto les aplica el ratio de descuento deacuerdo a
+    * la cantidad y este resultado lo multiplica por el monthAmount para obtener el
+    * precio final del paquete de meses que se desea adquirir
+    */
     @Override
     public BigDecimal getPrice() {
         BigDecimal bd = super.getPrice();
@@ -58,7 +83,12 @@ public class MontlyPayment extends Decorator {
         bd = bd.multiply(new BigDecimal(monthAmount));
         return bd.setScale(1, RoundingMode.HALF_UP);
     }
-
+    
+    /**
+    *
+    * @return: retorna la descripcion de la suscripcion deacuerdo a la cantidad de meses
+    * deseados en monthAmount, en caso de ser 0 se refiere a 15 dias.
+    */
     @Override
     public String getDescription() {
         String desc = super.getDescription();
@@ -81,5 +111,4 @@ public class MontlyPayment extends Decorator {
         }
         return desc;
     }
-
 }
