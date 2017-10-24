e3a8ef4c0455e7912ce41c5967a643390ed97c61
CesarCardozo <srfeudalwow@gmail.com>
2017-10-23 19:46:12 -0500

    Gradle Proyect

---
+++ PaginasWeb/GradleProyect/src/main/java/login/Login.java
@@ -0,0 +1,39 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		 
+		String user=request.getParameter("userName");
+		String pass=request.getParameter("userPassword");
+		 
+		if(user.equals("edwin")&&pass.equals("123")){
+			System.out.println("si entro la perra");
+			pw.println("Login Success...!");
+		} else {
+			pw.println("Login Failed...!");
+			pw.close();
+		}			
+	}
+
+}
